8ea09c65336bf7dbcce445028d9ddde3c53ecaac
zhangxp1993 <zhangxiuping@mail.kiz.ac.cn>
2025-12-06 14:23:34 +0800

    Create bash_paml_mfree.sh

---
+++ bash_paml_mfree.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -f "command_free_sh" ];then
+ rm -f command_free_sh
+fi
+ls cds_aln/*aln > og
+cat og | while read idd
+do
+id=${idd#*/}
+mkdir paml_result/${id%.aln} -p
+cp ./data/mfree_ctl paml_result/${id%.aln}
+cp ./cds_aln/${id} paml_result/${id%.aln}
+echo "cd paml_result/${id%.aln} && codeml mfree_ctl" >> command_free_sh
+done
+
+ParaFly -c xaa -CPU 28
+
+for i in $(cat data/free_immune); do ./scripts/find_paml_w.pl $i ; done
+for i in $(cat data/free_immune); do ./scripts/find_paml_speciesw.pl $i ; done
+./scripts/w2pca.pl og_nodes_w > og_nodes_pca
